f4c6cc734a6257e0e872761db359de0fcc533dca
J.J. Castro <jj.castro10@uniandes.edu.co>
2018-03-29 22:30:05 -0500

    More changes and cleanup

--- ARKitDemoA.playground/Contents.swift
+++ ARKitDemoA.playground/Contents.swift
@@ -5,23 +5,71 @@ import ARKit
 import PlaygroundSupport
 import SpriteKit
 
+extension SCNMaterial {
+    convenience init(name: String) {
+        self.init()
+        
+        self.lightingModel = .physicallyBased
+        self.diffuse.contents = UIImage(named: "Materials/\(name)/\(name)-albedo.png")
+        self.roughness.contents = UIImage(named: "Materials/\(name)/\(name)-roughness.png")
+        self.metalness.contents = UIImage(named: "Materials/\(name)/\(name)-metal.png")
+        self.normal.contents = UIImage(named: "Materials/\(name)/\(name)-normal.png")
+        
+        self.diffuse.wrapS = .repeat
+        self.diffuse.wrapT = .repeat
+        self.roughness.wrapS = .repeat
+        self.roughness.wrapT = .repeat
+        self.metalness.wrapS = .repeat
+        self.metalness.wrapT = .repeat
+        self.normal.wrapS = .repeat
+        self.normal.wrapT = .repeat
+    }
+}
+
+enum CollisionCategory: Int {
+    case bottom = 0b001
+    case cube   = 0b010
+    case plane  = 0b100
+}
+
 class Plane: SCNNode {
     
+    static let planeHeight = 0.01
     var planeGeometry: SCNPlane!
-//    var anchor: ARPlaneAnchor!
     
-    class func initWithThing(planeAnchor: ARPlaneAnchor) -> Plane {
+    class func initWithThing(planeAnchor: ARPlaneAnchor, hidden: Bool) -> Plane {
         
         let plane = Plane()
         
-        plane.geometry = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
-        plane.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "grid.png")!
+        let width = CGFloat(planeAnchor.extent.x)
+        let length = CGFloat(planeAnchor.extent.z)
+        
+        plane.geometry = SCNBox(width: width, height: CGFloat(Plane.planeHeight), length: length, chamferRadius: 0)
+        
+        let material = SCNMaterial()
+        let img = UIImage(named: "dotgrid.png")!
+        material.diffuse.contents = img
+        material.locksAmbientWithDiffuse = true
+
+        let transparentMat = SCNMaterial()
+        transparentMat.diffuse.contents = UIColor(white: 1.0, alpha: 0.0)
+        transparentMat.locksAmbientWithDiffuse = true
+
+        if hidden {
+            plane.geometry?.materials = [transparentMat, transparentMat, transparentMat, transparentMat, transparentMat, transparentMat]
+        } else {
+            plane.geometry?.materials = [transparentMat, transparentMat, transparentMat, transparentMat, material, transparentMat]
+        }
         
         let x = CGFloat(planeAnchor.center.x)
-        let y = CGFloat(planeAnchor.center.y)
+        let y = CGFloat(planeAnchor.center.y) - CGFloat(Plane.planeHeight / 2)
         let z = CGFloat(planeAnchor.center.z)
         plane.position = SCNVector3(x,y,z)
-        plane.eulerAngles.x = -.pi / 2
+//        plane.eulerAngles.x = -.pi / 2
+        
+        plane.physicsBody = SCNPhysicsBody(type: .kinematic, shape: SCNPhysicsShape(geometry: plane.geometry!, options: nil))
+        plane.physicsBody?.categoryBitMask = CollisionCategory.plane.rawValue
+        plane.physicsBody?.contactTestBitMask = 0
         
         plane.setTextureScale()
         
@@ -29,66 +77,41 @@ class Plane: SCNNode {
     }
     
     func update(planeAnchor: ARPlaneAnchor) {
-        var plane = self.geometry as! SCNPlane
+        let plane = self.geometry as! SCNBox
         
         let width = CGFloat(planeAnchor.extent.x)
-        let height = CGFloat(planeAnchor.extent.z)
+        let length = CGFloat(planeAnchor.extent.z)
         plane.width = width
-        plane.height = height
+        plane.length = length
         
         let x = CGFloat(planeAnchor.center.x)
-        let y = CGFloat(planeAnchor.center.y)
+        let y = CGFloat(planeAnchor.center.y) - CGFloat(Plane.planeHeight / 2)
         let z = CGFloat(planeAnchor.center.z)
         self.position = SCNVector3(x, y, z)
+        
+        self.physicsBody = SCNPhysicsBody(type: .kinematic, shape: SCNPhysicsShape(geometry: self.geometry!, options: nil))
         
         self.setTextureScale()
     }
     
     func setTextureScale() {
-        var geometry = self.geometry as! SCNPlane
+        var geometry = self.geometry as! SCNBox
         let width = geometry.width
-        let height = geometry.height
-        
-        let material = self.geometry?.firstMaterial
+        let height = geometry.length
+
+        let material = self.geometry?.materials[4]
         material?.diffuse.contentsTransform = SCNMatrix4MakeScale(Float(width), Float(height), Float(1))
         material?.diffuse.wrapS = .repeat
         material?.diffuse.wrapT = .repeat
     }
 }
 
-//: This is our Scene, which doesn't do a heck of a lot.
-public class Scene: SKScene {
-    
-    public override func didMove(to view: SKView) {
-        // Setup your scene here
-    }
-    
-    public override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
-    
-    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let sceneView = self.view as? ARSKView else {
-            return
-        }
-        
-        print("huh")
-        
-        if let touchLocation = touches.first?.location(in: sceneView) {
-            if let hit = sceneView.hitTest(touchLocation, types: .estimatedHorizontalPlane).first {
-                sceneView.session.add(anchor: ARAnchor(transform: hit.worldTransform))
-            }
-        }
-    }
-}
-
-
-class QIARViewController : UIViewController, ARSCNViewDelegate, ARSessionDelegate {
+class QIARViewController : UIViewController, ARSCNViewDelegate, SCNPhysicsContactDelegate {
     var sceneView: ARSCNView!
     var infoLabel = UILabel()
-    var node: SCNNode!
     
     var planes = [UUID: Plane]()
+    var boxes = [SCNNode]()
     
     override func loadView() {
         sceneView = ARSCNView(frame:CGRect(x: 0.0, y: 0.0, width: 500.0, height: 600.0))
@@ -109,50 +132,119 @@ class QIARViewController : UIViewController, ARSCNViewDelegate, ARSessionDelegat
         sceneView.delegate = self
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
+        sceneView.autoenablesDefaultLighting = true
+        
+        // Set the scene
         let scene = SCNScene() //(named: "omg.scn")!
         sceneView.scene = scene
+        
+        addBottomPlane()
+        sceneView.scene.physicsWorld.contactDelegate = self
 
         let config = ARWorldTrackingConfiguration()
         config.planeDetection = .horizontal
         
-        sceneView.session.delegate = self
-        
         self.view = sceneView
         sceneView.session.run(config)
-        
-        let plane = SCNPlane(width: 0.2, height: 0.2)
-        node = SCNNode(geometry: plane)
-        sceneView.scene.rootNode.addChildNode(node)
 
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.handleTap))
-        view.addGestureRecognizer(tapGesture)
+        sceneView.addGestureRecognizer(tapGesture)
+        
+        let holdGesture = UILongPressGestureRecognizer(target: self, action: #selector(self.handleLongPress))
+        holdGesture.minimumPressDuration = 0.5
+        sceneView.addGestureRecognizer(holdGesture)
     }
     
-    @objc func handleTap(gestureRecognize: UITapGestureRecognizer) {
-        guard let currentFrame = sceneView.session.currentFrame else {
+    
+    @objc func handleTap(recognizer: UITapGestureRecognizer) {
+        let tapPoint = recognizer.location(in: sceneView)
+        if let hit = sceneView.hitTest(tapPoint, types: .existingPlaneUsingExtent).first {
+            insertGeometry(hitResult: hit)
+        }
+    }
+    
+    @objc func handleLongPress(recognizer: UITapGestureRecognizer) {
+        if recognizer.state != .began {
             return
         }
         
-        let sphere = SCNSphere(radius: 0.05)
+        let tapPoint = recognizer.location(in: sceneView)
+        if let hit = sceneView.hitTest(tapPoint, types: .existingPlaneUsingExtent).first {
+            DispatchQueue.global().async {
+                self.explode(hitResult: hit)
+            }
+        }
+    }
+    
+    func addBottomPlane() {
+        let bottomPlane = SCNBox(width: 1000, height: 0.5, length: 1000, chamferRadius: 0)
         
-        let sphereNode = SCNNode(geometry: sphere)
-        sphereNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(geometry: sphere, options:nil))
-        sceneView.scene.rootNode.addChildNode(sphereNode)
+        let bottomMaterial = SCNMaterial()
+        bottomMaterial.diffuse.contents = UIColor(white: 1.0, alpha: 0.0)
+        bottomMaterial.locksAmbientWithDiffuse = true
+        bottomPlane.materials = [bottomMaterial]
         
-        var text = "\(currentFrame.camera.transform.columns.0.x) \(currentFrame.camera.transform.columns.1.x) \(currentFrame.camera.transform.columns.2.x) \(currentFrame.camera.transform.columns.3.x)"
-        text += "\n\(currentFrame.camera.transform.columns.0.y) \(currentFrame.camera.transform.columns.1.y) \(currentFrame.camera.transform.columns.2.y) \(currentFrame.camera.transform.columns.3.y)"
-        text += "\n\(currentFrame.camera.transform.columns.0.z) \(currentFrame.camera.transform.columns.1.z) \(currentFrame.camera.transform.columns.2.z) \(currentFrame.camera.transform.columns.3.z)"
+        let bottomNode = SCNNode(geometry: bottomPlane)
+        bottomNode.position = SCNVector3(0, -10, 0)
+        bottomNode.physicsBody = SCNPhysicsBody(type: .kinematic, shape: nil)
+        bottomNode.physicsBody?.categoryBitMask = CollisionCategory.bottom.rawValue
+        bottomNode.physicsBody?.contactTestBitMask = CollisionCategory.cube.rawValue
         
-        infoLabel.text = text
+        sceneView.scene.rootNode.addChildNode(bottomNode)
+    }
+    
+    func insertGeometry(hitResult: ARHitTestResult) {
+        let dimension = CGFloat(0.1)
+        let cube = SCNBox(width: dimension, height: dimension, length: dimension, chamferRadius: 0)
+        let node = SCNNode(geometry: cube)
         
-        let vector = SCNVector3(currentFrame.camera.transform.columns.3.x,
-                                currentFrame.camera.transform.columns.3.y,
-                                currentFrame.camera.transform.columns.3.z)
+        node.physicsBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(geometry: cube, options: nil))
+        node.physicsBody?.mass = 2.0
+        node.physicsBody?.categoryBitMask = CollisionCategory.cube.rawValue
+        node.physicsBody?.contactTestBitMask = 0
         
-        var translation = matrix_identity_float4x4
-        translation.columns.3.z = -0.3
-        sphereNode.simdTransform = matrix_multiply(currentFrame.camera.transform, translation)
-        sphereNode.runAction(SCNAction.move(by: vector, duration: TimeInterval(1)))
+        let insertionYOffset = Float(0.5)
+        node.position = SCNVector3(
+            hitResult.worldTransform.columns.3.x,
+            hitResult.worldTransform.columns.3.y + insertionYOffset,
+            hitResult.worldTransform.columns.3.z
+        )
+
+        boxes.append(node)
+        sceneView.scene.rootNode.addChildNode(node)
+    }
+    
+    func explode(hitResult: ARHitTestResult) {
+        print("exploded")
+        let explosionYOffset = Float(0.1);
+        
+        let position = SCNVector3(
+            hitResult.worldTransform.columns.3.x,
+            hitResult.worldTransform.columns.3.y - explosionYOffset,
+            hitResult.worldTransform.columns.3.z
+        )
+        
+        for cubeNode in boxes {
+            
+            var distance = SCNVector3(
+                cubeNode.worldPosition.x - position.x,
+                cubeNode.worldPosition.y - position.y,
+                cubeNode.worldPosition.z - position.z
+            )
+            
+            let len = sqrtf(distance.x * distance.x + distance.y * distance.y + distance.z * distance.z)
+            
+            let maxDistance = Float(2);
+            var scale = max(0, (maxDistance - len))
+            
+            scale = scale * scale * 2;
+            
+            distance.x = distance.x / len * scale;
+            distance.y = distance.y / len * scale;
+            distance.z = distance.z / len * scale;
+            
+            cubeNode.physicsBody?.applyForce(distance, at: SCNVector3(0.05, 0.05, 0.05), asImpulse: true)
+        }
     }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
@@ -177,6 +269,20 @@ class QIARViewController : UIViewController, ARSCNViewDelegate, ARSessionDelegat
 //        print("ugh")
 //    }
     
+    func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
+        let bitmask = contact.nodeA.physicsBody!.categoryBitMask | contact.nodeB.physicsBody!.categoryBitMask
+        
+        if bitmask == (CollisionCategory.cube.rawValue | CollisionCategory.bottom.rawValue) {
+            print(String(bitmask, radix: 2))
+//            print(contact.nodeB)
+            if (contact.nodeA.physicsBody?.categoryBitMask == CollisionCategory.cube.rawValue) {
+                contact.nodeA.removeFromParentNode()
+            } else {
+                contact.nodeB.removeFromParentNode()
+            }
+        }
+    }
+    
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
         print("ugh")
         if let plane = planes[anchor.identifier] {
@@ -189,7 +295,7 @@ class QIARViewController : UIViewController, ARSCNViewDelegate, ARSessionDelegat
             return
         }
 
-        let plane = Plane.initWithThing(planeAnchor: anchor as! ARPlaneAnchor)
+        let plane = Plane.initWithThing(planeAnchor: anchor as! ARPlaneAnchor, hidden: false)
         planes[anchor.identifier] = plane
         //sceneView.scene.rootNode
         node.addChildNode(plane)
